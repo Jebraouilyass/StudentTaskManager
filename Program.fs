cd103fd3e8358dff3882ae14af1819371e99607e
ilyass <AR9J5F@hallgato.uniduna.hu>
2025-05-25 13:17:42 +0200

    Update CORS for GitHub Pages

--- Program.fs
+++ Program.fs
@@ -1,18 +1,28 @@
 module Program
 
-open Microsoft.AspNetCore.Builder
-open Microsoft.Extensions.DependencyInjection
-open Microsoft.Extensions.Hosting
-open Giraffe
-open Handlers
-open Db
+     open Microsoft.AspNetCore.Builder
+     open Microsoft.Extensions.DependencyInjection
+     open Microsoft.Extensions.Hosting
+     open Giraffe
+     open Handlers
+     open Db
 
-[<EntryPoint>]
-let main args =
-    initDb() // Initialize the database
-    let builder = WebApplication.CreateBuilder(args)
-    builder.Services.AddGiraffe() |> ignore
-    let app = builder.Build()
-    app.UseGiraffe(webApp)
-    app.Run()
-    0
+     [<EntryPoint>]
+     let main args =
+         initDb() // Initialize the database
+         let builder = WebApplication.CreateBuilder(args)
+         builder.Services.AddGiraffe() |> ignore
+         builder.Services.AddCors(fun options ->
+             options.AddPolicy("AllowGitHubPages", fun builder ->
+                 builder
+                     .WithOrigins("https://Jebraouilyass.github.io")
+                     .AllowAnyMethod()
+                     .AllowAnyHeader()
+                 |> ignore
+             )
+         ) |> ignore
+         let app = builder.Build()
+         app.UseCors("AllowGitHubPages") |> ignore
+         app.UseGiraffe(webApp)
+         app.Run()
+         0
